bf009f47c4da5f96c90ecc330373df0b90f58273
Hiroyuki Anai <pirosikick@gmail.com>
2016-10-17 23:13:53 +0900

    Tour or Heroes pt6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { HeroService } from "./hero.service";
 
 import { AppComponent }   from './app.component';
+import { HeroesComponent }   from './heroes.component';
 import { HeroDetailComponent }   from './hero-detail.component';
+import { DashboardComponent }   from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  providers: [HeroService],
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent,
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
