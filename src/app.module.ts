c61dcfed136c60a559bb145be407e0d0c8350524
Hiroyuki Anai <pirosikick@gmail.com>
2016-10-18 22:52:48 +0900

    Tour of Heroes pt7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroService } from "./hero.service";
 
@@ -9,6 +15,7 @@ import { AppComponent }   from './app.component';
 import { HeroesComponent }   from './heroes.component';
 import { HeroDetailComponent }   from './hero-detail.component';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroSearchComponent }   from './hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -17,6 +24,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule,
   ],
   declarations: [
@@ -24,6 +33,7 @@ import { AppRoutingModule } from './app-routing.module';
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
   ],
   bootstrap: [ AppComponent ]
 })
